a1ade9b34dee2a7e7e1fe1625623f4515bc86491
Aezart <heroofhyla@gmail.com>
2016-04-25 12:01:56 -0700

    Create KeyboardManager

--- Speed Snake II/src/KeyboardManager.java
+++ Speed Snake II/src/KeyboardManager.java
@@ -1,4 +1,48 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.HashMap;
+import java.util.LinkedList;
 
-public class KeyboardManager {
+public class KeyboardManager implements KeyListener{
+	
+	HashMap<Integer, Boolean> keyIsHeld = new HashMap<>();
+	LinkedList<Integer> keyPressQueue = new LinkedList<>();
+	LinkedList<Integer> keyHoldStack = new LinkedList<>();
+	
+	@Override
+	public void keyPressed(KeyEvent k) {
+		if (!(keyIsHeld.containsKey(k.getKeyCode()) && !keyIsHeld.get(k.getKeyCode()))){
+			keyPressQueue.addLast(k.getKeyCode());
+			keyHoldStack.addLast(k.getKeyCode());
+		}
+		keyIsHeld.put(k.getKeyCode(), true);
+		
+	}
 
+	@Override
+	public void keyReleased(KeyEvent k) {
+		keyIsHeld.put(k.getKeyCode(), false);
+		keyHoldStack.remove(k.getKeyCode());
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// nothing
+	}
+	
+	public int getKey(){
+		if (keyPressQueue.isEmpty()){
+			while (!keyHoldStack.isEmpty()){
+				if (keyIsHeld.get(keyHoldStack.getLast())){
+					return keyHoldStack.getLast();
+				}else{//we shouldn't reach this state, but it's here just in case
+					keyHoldStack.removeLast();
+				}
+			}
+			return KeyEvent.VK_UNDEFINED;
+		}
+		else{
+			return keyPressQueue.removeFirst();
+		}
+	}
 }
