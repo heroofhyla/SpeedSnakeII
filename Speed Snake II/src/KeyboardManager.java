7209397e93437059ab391cd77eda069aefa8b450
Aezart <heroofhyla@gmail.com>
2016-04-25 18:19:44 -0700

    Add filter to KeyboardManager to only track expected keys

--- Speed Snake II/src/KeyboardManager.java
+++ Speed Snake II/src/KeyboardManager.java
@@ -1,5 +1,6 @@
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -9,23 +10,36 @@ public class KeyboardManager implements KeyListener{
 	LinkedList<Integer> keyPressQueue = new LinkedList<>();
 	LinkedList<Integer> keyHoldStack = new LinkedList<>();
 	
+	ArrayList<Integer> validKeys = new ArrayList<>();
+	
+	public KeyboardManager(){
+		int[] validKeysArray = {KeyEvent.VK_UP, KeyEvent.VK_DOWN, KeyEvent.VK_LEFT, KeyEvent.VK_RIGHT, 
+				KeyEvent.VK_W, KeyEvent.VK_A, KeyEvent.VK_S, KeyEvent.VK_D, KeyEvent.VK_H, KeyEvent.VK_ENTER};
+		for (int i = 0; i < validKeysArray.length; ++i){
+			validKeys.add(validKeysArray[i]);
+		}
+	}
 	@Override
 	public void keyPressed(KeyEvent k) {
-		if ((!keyIsHeld.containsKey(k.getKeyCode()) || !keyIsHeld.get(k.getKeyCode()))){
-			keyPressQueue.addLast(k.getKeyCode());
-			keyHoldStack.addLast(k.getKeyCode());
-			System.out.println("Keycode:" + k.getKeyCode() + " pressed");
+		if (validKeys.contains(k.getKeyCode())){
+			if ((!keyIsHeld.containsKey(k.getKeyCode()) || !keyIsHeld.get(k.getKeyCode()))){
+				keyPressQueue.addLast(k.getKeyCode());
+				keyHoldStack.addLast(k.getKeyCode());
+				System.out.println("Keycode:" + k.getKeyCode() + " pressed");
+			}
+			keyIsHeld.put(k.getKeyCode(), true);
 		}
-		keyIsHeld.put(k.getKeyCode(), true);
 		
 	}
 
 	@Override
 	public void keyReleased(KeyEvent k) {
-		System.out.println("Keycode:" + k.getKeyCode() + " released");
-		keyIsHeld.put(k.getKeyCode(), false);
-		if (keyHoldStack.contains(k.getKeyCode())){
-			keyHoldStack.remove((Integer)(k.getKeyCode()));
+		if (validKeys.contains(k.getKeyCode())){
+			System.out.println("Keycode:" + k.getKeyCode() + " released");
+			keyIsHeld.put(k.getKeyCode(), false);
+			if (keyHoldStack.contains(k.getKeyCode())){
+				keyHoldStack.remove((Integer)(k.getKeyCode()));
+			}
 		}
 	}
 
