11a423f3827227297fb8a658270f1ca4ce4198cf
Aezart <heroofhyla@gmail.com>
2016-04-26 23:16:16 -0700

    Scene work

---
+++ Speed Snake II/src/GameScene.java
@@ -0,0 +1,10 @@
+import java.awt.Graphics2D;
+
+public class GameScene extends SnakeScene{
+	
+	@Override
+	public void tick(Graphics2D g, int key) {
+		//TODO
+		
+	}
+}
---
+++ Speed Snake II/src/HighScoreScene.java
@@ -0,0 +1,11 @@
+import java.awt.Graphics2D;
+
+public class HighScoreScene extends SnakeScene{
+
+	@Override
+	public void tick(Graphics2D g, int key) {
+		//TODO
+		
+	}
+
+}
