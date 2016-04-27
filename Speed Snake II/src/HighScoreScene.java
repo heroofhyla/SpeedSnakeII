41dd9497c5067ec6642b982be157aad5eee6a998
Aezart <heroofhyla@gmail.com>
2016-04-27 15:55:55 -0700

    Begin file IO work for HighScoreScene

--- Speed Snake II/src/HighScoreScene.java
+++ Speed Snake II/src/HighScoreScene.java
@@ -1,15 +1,30 @@
 import java.awt.Graphics2D;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class HighScoreScene extends SnakeScene{
 
-	
+	ArrayList<Integer> scores;
 	public HighScoreScene(int startingScore) {
+		File scoresFile = new File("highscores.txt");
+		if (scoresFile.exists()){
+			System.out.println("File exists");
+			//TODO
+		}
+		try (FileWriter writer = new FileWriter("highscores.txt")){
+			writer.write("Test");
+			System.out.println("File written");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// TODO
 	}
 
 	@Override
 	public void tick(Graphics2D g, int key) {
-		//TODO
 		
 	}
 
