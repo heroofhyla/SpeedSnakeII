4798b859b7e5253daf6681b8af41bf84f67251c0
Ryan <Ryan@RyanHorner-HP>
2016-04-27 19:21:09 -0700

    awdawd

--- Speed Snake II/src/Snake.java
+++ Speed Snake II/src/Snake.java
@@ -1,4 +1,27 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.JFrame;
 
 public class Snake {
-	//TODO
+	public JFrame jframe;
+	public Toolkit toolkit;
+	
+	public static Snake snake;
+	
+	public Snake(){
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		jframe = new JFrame("Snake"); // declaring the instance of jframe
+		jframe.setVisible(true);
+		jframe.setSize(500, 500);
+		// jframe.setTitle("ENTER-NEW-GAMENAME-HERE");
+		jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.height / 2 - jframe.getHeight() / 2); // center of screen
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args){
+		snake = new Snake();
+		// ignore this comment
+	}
+	
+	
 }
