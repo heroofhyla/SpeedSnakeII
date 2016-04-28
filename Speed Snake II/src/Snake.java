63aa6a930e8b492905f83474868731ce68571133
Ryan <Ryan@RyanHorner-HP>
2016-04-28 00:12:53 -0700

    attemptStep 00

--- Speed Snake II/src/Snake.java
+++ Speed Snake II/src/Snake.java
@@ -1,27 +1,34 @@
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import javax.swing.JFrame;
+// attemptStep(KeyEvent key)
+
+import java.awt.event.KeyEvent;
 
 public class Snake {
-	public JFrame jframe;
-	public Toolkit toolkit;
+	public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
 	
-	public static Snake snake;
+	public int direction = DOWN;
 	
-	public Snake(){
-		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		jframe = new JFrame("Snake"); // declaring the instance of jframe
-		jframe.setVisible(true);
-		jframe.setSize(500, 500);
-		// jframe.setTitle("ENTER-NEW-GAMENAME-HERE");
-		jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.height / 2 - jframe.getHeight() / 2); // center of screen
-		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public void attemptStep(KeyEvent e)
+	{
+		int i = e.getKeyCode();
+
+		if ((i == KeyEvent.VK_A || i == KeyEvent.VK_LEFT) && direction != RIGHT)
+		{
+			direction = LEFT;
+		}
+
+		if ((i == KeyEvent.VK_D || i == KeyEvent.VK_RIGHT) && direction != LEFT)
+		{
+			direction = RIGHT;
+		}
+
+		if ((i == KeyEvent.VK_W || i == KeyEvent.VK_UP) && direction != DOWN)
+		{
+			direction = UP;
+		}
+
+		if ((i == KeyEvent.VK_S || i == KeyEvent.VK_DOWN) && direction != UP)
+		{
+			direction = DOWN;
+		}
 	}
-	
-	public static void main(String[] args){
-		snake = new Snake();
-		// ignore this comment
-	}
-	
-	
 }
